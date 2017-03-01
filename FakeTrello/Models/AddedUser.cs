29a893e3a25b0ea4ee0a3716e301c302da05ade7
blairvansant <blairbot@gmail.com>
2017-02-28 21:33:51 -0600

    added all classes for fake trello according to my EDR

---
+++ FakeTrello/Models/AddedUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class AddedUser
+    {
+        [Key]
+        public int AddedUserId { get; set; }
+
+        public string AddedUserName { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
