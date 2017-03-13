e1f415beae77e4e1f3ef5346c25161a988e773c4
blairvansant <blairbot@gmail.com>
2017-03-13 18:56:24 -0500

    add first tests ensuring context

---
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeTrello.DAL;
+
+namespace FakeTrello.Tests.DAL
+{
+    [TestClass]
+    public class FakeTrelloRepositoryTests
+    {
+        public object Context { get; private set; }
+
+        [TestMethod]
+        public void EnsureIHaveNotNullContext()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureICanInjectContextInstance() 
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+            Assert.IsNotNull(repo.Context);
+        }
+    }
+}
