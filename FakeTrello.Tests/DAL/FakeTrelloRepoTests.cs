6d320bd6d20b11015d76f08b0735b4d8eba025d0
blairvansant <blairbot@gmail.com>
2017-03-14 09:26:00 -0500

    end of class

--- FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -14,6 +14,18 @@ namespace FakeTrello.Tests.DAL
     {
         public object Context { get; private set; }
 
+        public void CreateFakeDatabase()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnsureICanCreateAnInstance()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+        }
+
         [TestMethod]
         public void EnsureIHaveNotNullContext()
         {
@@ -67,5 +79,62 @@ namespace FakeTrello.Tests.DAL
             //assert
             Assert.AreEqual(repo.Context.Boards.Count(), 1);
         }
+
+        [TestMethod]
+        public void EnsureICanReturnBoards()
+        {
+            List<Board> fake_board_table = new List<Board>();
+            {
+                new Board { Name = "MyBoard" };
+            }
+
+            var query_boards = fake_board_table.AsQueryable();//re-express this list as something that understands queries
+
+            Mock<FakeTrelloContext> fake_context = new Mock<FakeTrelloContext>();
+            Mock<DbSet<Board>> mock_boards_set = new Mock<DbSet<Board>>();
+
+            //hey LINQ, use the provider from our fake board/table list
+            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Provider).Returns(query_boards.Provider);
+            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.Expression).Returns(query_boards.Expression);
+            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.ElementType).Returns(query_boards.ElementType);
+            mock_boards_set.As<IQueryable<Board>>().Setup(b => b.GetEnumerator()).Returns(() => query_boards.GetEnumerator());
+
+            mock_boards_set.Setup(b => b.Add(It.IsAny<Board>())).Callback((Board board) => fake_board_table.Add(board));
+            fake_context.Setup(c => c.Boards).Returns(mock_boards_set.Object);//context.boards returns fake_board_table (a list)
+
+
+
+            Mock<FakeTrelloContext> mock_context = new Mock<FakeTrelloContext>();
+            FakeTrelloRepository repo = new FakeTrelloRepository(mock_context.Object);
+
+            //act
+            int expected_board_count = 1;
+            int actual_board_count = repo.Boards.Count();
+
+            Assert.AreEqual(expected_board_count, actual_board_count);
+
+           }
+
+        [TestMethod]
+        public void EnsureICanFindABoard()
+        {
+            //arrange
+            fake_board_table.Add(new Board { BoardId = 1, Name = "My Board" });
+            CreateFakeDatabase();
+
+            //act
+            string expected_board_name = "My Board";
+            string actual_board_name = repo.GetBoard(1).Name;
+
+            //assert
+            Assert.AreEqual(expected_board_name, actual_board_name);
+        }
+
+        }
+
     }
-}
+
+
+
+
+
